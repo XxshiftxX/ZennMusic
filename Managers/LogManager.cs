2f38b678e95d02fc86ae31967f64d1ea1fecbbbf
shift <qjfrntop12@naver.com>
2019-08-19 11:54:40 +0900

    Implement LogManager class and method decorator

--- Managers/LogManager.cs
+++ Managers/LogManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,54 @@ using System.Threading.Tasks;
 
 namespace ZennMusic.Managers
 {
-    static class LogManager
+    public static class LogManager
     {
-        // NotImplementedException 쓰고싶은데 당장 테스트할때 에러나니까 그냥 빈 메서드로 선언함 ^^
-        public static void Initialize() { }
-        public static void Log(string message, bool hasTimestamp = true) { }
+        private static FileStream _logger;
+
+        private string LogFileDirectory => Path.Combine(Directory.GetCurrentDirectory(), "Log");
+        private static string CurrentTimeString => DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:fff");
+
+        public static void Initialize() 
+        {
+            var dateString = DateTime.Now.ToString("yyyy-MM-dd");
+            var LogFilePath = $@"{LogFileDirectory}\ZennLog {dateString}.txt";
+
+            if (!Directory.Exists(LogFileDirectory))
+                Directory.CreateDirectory(LogDirectory);
+            if (!File.Exists(LogFilePath))
+                File.Create(LogFilePath).Close();
+
+            _logger = File.Open(LogFilePath, FileMode.Append, FileAccess.Write, FileShare.Read);
+            
+            Log("[Logger Started]");
+        }
+        public static void Log(string message, bool hasTimestamp = true) 
+        {
+            if (hasTimestamp)
+                message += $"[{CurrentTimeString}] ";
+            
+            var messageBytes = Encoding.UTF8.GetBytes(message + "\n");
+            _logger.Write(messageBytes, 0, messageBytes.Length);
+            _logger.Flush();
+        }
+    }
+
+    public class InterceptorAttribute : Attribute, IMethodDecorator
+    {
+        public MethodBase Method;
+        public object[] Arguments;
+        public string ArgumentString => string.Join(", ", Arguments);
+
+        public void Init(object instance, MethodBase method, object[] args)
+        {
+            Method = method;
+            Arguments = args;
+        }
+
+        public void OnEntry()
+            => LogManager.Log($"[Method Entry] {Method.Name}[{GetHashCode()}] >> ({ArgumentString})");
+
+        public void OnExit()
+            => LogManager.Log($"[Method Exit]  {Method.Name}[{GetHashCode()}]");
     }
 }
